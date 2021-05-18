26842511c6d63ea3f28bb00ad5148fd5f9b3d44f
zafar <zafarhussain633@gmail.com>
2021-05-18 12:32:41 +0530

    footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <footer className="footer">
-            created by zafar
+            created by zafar hussain
         </footer>
     )
 }
